ab8f4a52e1f399f2b7e16f101fcdf8f31c8dc5a3
maxaj <maxaj@Xubuntu.myguest.virtualbox.org>
2023-02-10 14:27:08 +0100

    Set up node class

---
+++ js_file.js
@@ -0,0 +1,21 @@
+class Node {
+value = null;
+nextNode = null;
+
+    constructor(value) {
+        this.value = value
+    }
+    
+    value() {
+        return this.value;
+    }
+}
+
+class LinkedList {
+
+
+    append(value) {
+
+    }
+}
+

5d670ddbb14a9786a31038dc1cf54e4c1dbf44b0
maxaj <maxaj@Xubuntu.myguest.virtualbox.org>
2023-02-13 00:20:13 +0100

    Add tail property to linked list class and add setter and getter to Node class

--- js_file.js
+++ js_file.js
@@ -3,20 +3,28 @@ class Node {
         this.value = value;
         this.nextNode = nextNode;
     }
+    get nextNode () {
+        return this.nextNode;
+    }
+    set nextNode (node) {
+        this.nextNode = node;
+    }
 }
 
 class LinkedList {
     constructor(value = null) {
         this.head = new Node(value);
         this.size = 0;
+        this.tail = this.head;
     }
 
     append (value) {
         let currentNode = this.head;
+        this.tail = new Node(value);
         while (currentNode.nextNode !== null) {
             currentNode = currentNode.nextNode;
         }
-        currentNode.nextNode = new Node(value);
+        currentNode.nextNode = this.tail;
         size++;
     }
 }
